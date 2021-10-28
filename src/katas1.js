6ab8772af847124e2c4f3ca316087941f66661b3
Wallace Junior <wallaceoliveirarn@gmail.com>
2021-10-28 16:30:18 -0300

    fazendo correcao da tarefa

--- src/katas1.js
+++ src/katas1.js
@@ -1,116 +1,119 @@
 function oneThroughTwenty(num) {
-    
-  for (let number = 1; number <= num; number++){
-    console.log(number);
+
+  let retornaNumeros = [];
+  for (let number = 1; number <= num; number++) {
+    retornaNumeros.push(number);
   }
+  return retornaNumeros;
 }
-   oneThroughTwenty(20);
-      
+console.log(oneThroughTwenty(20));
+
 
 function evensToTwenty(num) {
-    
-   let contador = 1 ;
-    while (contador <= num) {
-        let resto = contador % 2;
-        if (resto == 0){
-            console.log(contador);
-        }
-        contador++;
-    }
-}
-evensToTwenty(20);
 
-   
-function oddsToTwenty(num) {
-    
-let contador = 1 ;
-    while (contador <= num) {
-        let resto = contador % 2;
-        if (resto == 1){
-            console.log(contador);
-        }
-        contador++;
+  let retornaNumeros2 = [];
+  let contador = 1;
+  for (let contador = 1; contador <= num; contador++) {
+    if (contador % 2 === 0) {
+      retornaNumeros2.push(contador)
     }
+  }
+  return retornaNumeros2;
+
+} console.log(evensToTwenty(20));
+
+
+function oddsToTwenty(num) {
+
+  let retornaNumeros3 = [];
+
+  for (let contador = 1; contador <= num; contador++) {
+    if (contador % 2 === 1) {
+      retornaNumeros3.push(contador)
+    }
+  }
+  return retornaNumeros3;
+
 }
-oddsToTwenty(20)
+console.log(oddsToTwenty(20));
+
 
 function multiplesOfFive(num) {
-    
- let contador = 1 ;
-    while (contador <= num) {
-        let resto = contador % 5;
-        if (resto == 0){
-            console.log(contador);
-        }
-        contador++;
+
+  let retornaNumeros4 = [];
+
+  for (let contador = 1; contador <= num; contador++) {
+    if (contador % 5 === 0) {
+      retornaNumeros4.push(contador)
     }
-}
-multiplesOfFive(100);
-    
+
+  }
+  return retornaNumeros4;
+} console.log(multiplesOfFive(100));
+
 
 function squareNumbers(num) {
+  let retornaNumeros5 = [];
+
   let contador = 1;
-  for (let numero = 1 ; numero <= 10; numero++){
+  for (let numero = 1; numero <= 10; numero++) {
     let quadPerf = numero * numero;
-    console.log(quadPerf);
-  }
-}
-
-squareNumbers(100)
-
-function squareNumbers
-
+    retornaNumeros5.push(quadPerf)
+  } return retornaNumeros5
+} console.log(squareNumbers(100))
+// ok
 function countingBackwards(num) {
-    let contador = 20;
-        for (let numero = contador - 1 ; numero >= 1; numero--){
-         console.log(numero);
+  let retornaNumeros6 = [];
+  let contador = num;
+  for (let numero = contador; numero >= 1; numero--) {
+    retornaNumeros6.push(numero)
+  } return retornaNumeros6
+} console.log(countingBackwards(20))
 
-}
-}
-countingBackwards(20)
-    
+function evenNumbersBackwards(num) {
+  let contador = num;
+  let retornaNumeros7 = [];
 
-function retornaPares(num){
-let contador = num;
-  for (let numero = contador ; numero >= 1; numero--){
-	if (numero % 2 === 0){
-		console.log(numero);
+  for (let numero = contador; numero >= 1; numero--) {
+    if (numero % 2 === 0) {
+      retornaNumeros7.push(numero)
+    }
+  } return retornaNumeros7
 }
+console.log(evenNumbersBackwards(20))
 
-}
-}
-retornaPares(20)
 
-function retornaPares(num){
-let contador = num;
-  for (let numero = contador ; numero >= 1; numero--){
-	if (numero % 2 === 1){
-		console.log(numero);
+function oddNumbersBackwards(num) {
+  let retornaNumeros8 = [];
+  contador = num;
+  for (let numero = contador; numero >= 1; contador--) {
+    if (contador % 2 === 1) {
+      retornaNumeros8.push(contador)
+    }
+  }
+  return retornaNumeros8;
 }
-
-}
-}
-retornaPares(20)
+console.log(oddNumbersBackwards(20));
 
 function multiplesOfFiveBackwards(num) {
-let contador = num ;
-   for (let numero = contador; numero >= 1; numero--){
-        let resto = numero % 5;
-        if (resto == 0){
-            console.log(numero);
-        }
-        contador++;
+  let retornaNumeros9 = [];
+  let contador = num;
+  for (let numero = contador; numero >= 1; numero--) {
+    if (numero % 5 === 0) {
+
+      retornaNumeros9.push(numero)
     }
-}
-multiplesOfFiveBackwards(100);
+  } return retornaNumeros9
+} console.log(multiplesOfFiveBackwards(100));
 
 function squareNumbersBackwards(num) {
-  
-  let contador = num;
-  for (let numero = 10 ; numero >= 1; numero--){
-    let quadPerf = numero * numero;
-    console.log(quadPerf);
-  }
-}
+  let retornaNumeros10 = [];
 
-squareNumbersBackwards(100)
+  let contador = num;
+  for (let numero = 10; numero >= 1; numero--) {
+    let quadPerf = numero * numero;
+    retornaNumeros10.push(quadPerf)
+
+  }
+  return retornaNumeros10;
+} console.log(squareNumbersBackwards(100));
